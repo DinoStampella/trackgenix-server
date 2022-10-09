98ca1f616089b25799d2654cac247b95fc8732b2
Luciana Prates <lucianaprates10@gmail.com>
2022-10-09 16:43:11 -0300

    TG-08: added create task

---
+++ src/resources/tasks.js
@@ -0,0 +1,38 @@
+import express from 'express';
+
+const fs = require('fs');
+
+const router = express.Router();
+
+const tasks = require('../data/tasks.json');
+
+router.post('/add', (req, res) => {
+  const task = req.body;
+  const { taskName } = task;
+  const taskDescription = task.description;
+  const ids = tasks.map((t) => t.id);
+  const maxId = Math.max(...ids);
+  const newTask = {
+    id: maxId + 1,
+    taskName,
+    description: taskDescription,
+  };
+  if (!taskName && !taskDescription) {
+    res.send('Not exist name and description');
+  } else if (!taskName) {
+    res.send('Not exist name');
+  } else if (!taskDescription) {
+    res.send('Not exist description');
+  } else {
+    tasks.push(newTask);
+  }
+  fs.writeFile('src/data/tasks.json', JSON.stringify(tasks), (err) => {
+    if (err) {
+      res.send('Cannot save new task');
+    } else {
+      res.send('User Created');
+    }
+  });
+});
+
+export default router;
