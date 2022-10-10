58a46660ef2becbc78f69500ce7442200ba1ec53
Francisco Santarelli <franciscosantarelli@gmail.com>
2022-10-10 01:10:09 -0300

    TG : Add get by Id and by Username

--- src/resources/admins.js
+++ src/resources/admins.js
@@ -7,4 +7,22 @@ router.get('/getAll', (req, res) => {
   res.send(admins);
 });
 
+router.get('/get/:id', (req, res) => {
+  const adminId = req.params.id;
+  const adminObtained = admins.find((admin) => admin.id === adminId);
+  if (adminObtained) {
+    res.send(adminObtained);
+  } else {
+    res.send('Admin not found');
+  }
+});
+router.get('/getByUsername/:username', (req, res) => {
+  const adminUsername = req.params.username;
+  const adminObtained = admins.find((admin) => admin.user_name === adminUsername);
+  if (adminObtained) {
+    res.send(adminObtained);
+  } else {
+    res.send('Username of admin not found');
+  }
+});
 module.exports = router;
