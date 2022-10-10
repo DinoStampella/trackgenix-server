531b4e54f7fa8b66e476d829c87187ac5c1f5b77
melissabonillasellanes <melibonilla3@gmail.com>
2022-10-10 20:15:25 -0300

    TG: Added put method

--- src/resources/employees.js
+++ src/resources/employees.js
@@ -4,17 +4,57 @@ const employees = require("../data/employees.json");
 const router = express.Router();
 
 router.delete("/delete/:id", (req, res) => {
-    const idEmployee = req.params.id.toString()
-    console.log(idEmployee)
+    const idEmployee = parseInt(req.params.id)
     const filteredEmployee = employees.filter((employee) => employee.id !== idEmployee)
-    console.log(filteredEmployee);
+    const deleteEmloyee = employees.find((employee) => employee.id === idEmployee)
+    if(deleteEmloyee) {
     fs.writeFile("src/data/employees.json", JSON.stringify(filteredEmployee), (err) => {
         if(err) {
-            res.send("Unable to delete employee")
+            res.status(400).json({
+                success: false
+            });
         } else {
-            res.send("Eliminated employee")
+            res.status(200).json({
+                success: true,
+                msg: "Employee deleted successfully",
+                data: employees
+        });
         }
     })
+}else {
+        res.status(404).json({
+            success: false,
+            msg: "There is no Employee with this id"
+        });
+    }
 });
+
+router.put("/put/:id", (req, res) => {
+    const idEmployee = parseInt(req.params.id)
+    const employee = employees.find((employee) => employee.id === idEmployee)
+    if(employee) {
+        const index = employees.indexOf(employee);
+        const newEmployee = req.body;
+        employees[index] = newEmployee;
+        fs.writeFile("src/data/employees.json", JSON.stringify(employees), (err) => {
+            if(err) {
+                res.status(400).json({
+                    success: false
+                });
+            } else {
+                res.status(200).json({
+                    success: true,
+                    msg: "Employee modified successfully",
+                    data: employees
+                });
+            }
+        });
+    } else {
+        res.status(404).json({
+            success: false,
+            msg: "There is no Employee with this id"
+            });
+    }
+})
 
 module.exports = router;
