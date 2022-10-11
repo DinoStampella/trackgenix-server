bef44b843a07c6300dd678df564ff199d55abdf8
Luciana Prates <lucianaprates10@gmail.com>
2022-10-11 19:50:14 -0300

    TG-08: correction in the response

--- src/resources/tasks.js
+++ src/resources/tasks.js
@@ -8,9 +8,9 @@ router.get('/getById/:id', (req, res) => {
   const taskId = parseInt(req.params.id, 10);
   const foundTask = tasks.find((task) => task.id === taskId);
   if (foundTask) {
-    res.send({ success: true, data: foundTask });
+    res.status(200).send({ success: true, msg: 'Task found successfully', data: foundTask });
   } else {
-    res.status(404).send({ success: false, msg: 'Task found successfully' });
+    res.status(404).send({ success: false, msg: 'There is no task with this id' });
   }
 });
 
