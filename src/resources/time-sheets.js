488efac054be19bf772aa1312fc5d3e8d15c6c8e
Maicol Freire <maicolfreire001@gmail.com>
2022-10-11 06:32:49 -0300

    TG: edited stringify

--- src/resources/time-sheets.js
+++ src/resources/time-sheets.js
@@ -8,7 +8,7 @@ router.post('/add', (req, res) => {
   const newTimesheet = req.body;
   newTimesheet.id = (timeSheets.length + 1).toString();
   timeSheets.push(newTimesheet);
-  fs.writeFile('src/data/time-sheets.json', JSON.stringify(timeSheets), (err) => {
+  fs.writeFile('src/data/time-sheets.json', JSON.stringify(timeSheets, null, 2), (err) => {
     if (err) {
       return res.status(400).json({
         success: false,
