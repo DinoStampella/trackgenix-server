ef50702dc3babb76323c4ea49ff9c292af3271b8
Maicol Freire <maicolfreire001@gmail.com>
2022-10-11 05:51:51 -0300

    TG: added response status

--- src/resources/time-sheets.js
+++ src/resources/time-sheets.js
@@ -4,7 +4,11 @@ const timeSheets = require('../data/time-sheets.json');
 const router = express.Router();
 
 router.get('/get', (req, res) => {
-  res.send(timeSheets);
+  res.status(200).json({
+    success: true,
+    msg: 'Timesheet found successfully',
+    data: timeSheets
+  });
 });
 
 module.exports = router;
