eb4aacb3fd38b5d26307c20d46c3c872d35150b5
Maicol Freire <maicolfreire001@gmail.com>
2022-10-11 05:40:22 -0300

    TG: edited return statuses

--- src/resources/time-sheets.js
+++ src/resources/time-sheets.js
@@ -6,24 +6,20 @@ const router = express.Router();
 
 router.post('/add', (req, res) => {
   const newTimesheet = req.body;
-  let alreadyExist = false;
-  timeSheets.forEach((timeSheet) => {
-    if (timeSheet.id === newTimesheet.id) {
-      alreadyExist = true;
+  newTimesheet.id = (timeSheets.length + 1).toString();
+  timeSheets.push(newTimesheet);
+  fs.writeFile('src/data/time-sheets.json', JSON.stringify(timeSheets), (err) => {
+    if (err) {
+      return res.status(400)({
+        success: false,
+      });
     }
-  });
-  if (!alreadyExist) {
-    timeSheets.push(newTimesheet);
-    fs.writeFile('src/data/time-sheets.json', JSON.stringify(timeSheets), (err) => {
-      if (err) {
-        res.send("Timesheet couldn't be saved");
-      } else {
-        res.send('Timesheet created');
-      }
+    return res.status(201)({
+      success: true,
+      msg: 'Timesheet created successfully',
+      data: newTimesheet,
     });
-  } else {
-    res.send('Already exist a timesheet with that id');
-  }
+  });
 });
 
 module.exports = router;
