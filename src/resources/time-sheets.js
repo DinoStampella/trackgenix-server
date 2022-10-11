51df37f4d0e4184e8fc44b0a86d2590920d3f52f
Maicol Freire <maicolfreire001@gmail.com>
2022-10-11 05:48:26 -0300

    TG: edited return statuses

--- src/resources/time-sheets.js
+++ src/resources/time-sheets.js
@@ -7,9 +7,16 @@ router.get('/getById/:id', (req, res) => {
   const timeSheetId = req.params.id;
   const timeSheetObtained = timeSheets.find((timeSheet) => timeSheet.id === timeSheetId);
   if (timeSheetObtained) {
-    res.send(timeSheetObtained);
+    res.status(200).json({
+      success: true,
+      msg: "Timesheet found successfully",
+      data: timeSheetObtained
+    })
   } else {
-    res.send('Timesheet not found');
+    res.status(404).json({
+      success: false,
+      msg: "Cannot found timesheet"
+    })
   }
 });
 
