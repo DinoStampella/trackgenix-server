116995b6e8dab1691fa38d2c27a8aa4c1cb8882f
Luciana Prates <lucianaprates10@gmail.com>
2022-10-10 23:34:43 -0300

    TG-08: corrections after review

--- src/resources/tasks.js
+++ src/resources/tasks.js
@@ -8,9 +8,9 @@ router.get('/getById/:id', (req, res) => {
   const taskId = parseInt(req.params.id, 10);
   const foundTask = tasks.find((task) => task.id === taskId);
   if (foundTask) {
-    res.send(foundTask);
+    res.send({ success: true, data: foundTask });
   } else {
-    res.status(404).send('{success: false,msg: "Task not found"}');
+    res.status(404).send({ success: false, msg: 'Task not found' });
   }
 });
 
