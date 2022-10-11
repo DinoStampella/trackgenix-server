815c343893f92fa2d31edf0e49be84ac86237946
Erick Sequeira <ericknz2014@gmail.com>
2022-10-10 21:17:41 -0300

    TG: clean code

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
-const express = require ('express');
+// use 'import' to import libraries
+const express = require('express');
 
 const app = express();
 const employeesRouter = require('./resources/employees');
+
 app.use(express.json());
 app.use('/employees', employeesRouter);
 
-// use "import" to import libraries
-//import express from 'express';
+// import express from 'express';
 
-// use "require" to import JSON files
+// use 'require' to import JSON files
 const admins = require('./data/admins.json');
 
-//const app = express();
+// const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', async (req, res) => {
@@ -28,5 +29,3 @@ app.listen(port, () => {
   // eslint-disable-next-line no-console
   console.log(`Example app listening on port ${port}`);
 });
-
-
--- src/resources/employees.js
+++ src/resources/employees.js
@@ -1,48 +1,53 @@
-const express = require ('express');
-const employees = require('../data/employees.json');
-const router = express.Router();
+const express = require('express');
+
 const fs = require('fs');
-router.use(express.urlencoded({extended: false}));
+
+const employees = require('../data/employees.json');
+
+const router = express.Router();
+
+router.use(express.urlencoded({ extended: false }));
 
 router.get('/getAll', (req, res) => {
-    res.send(employees)
+  res.send(employees);
 });
 
 router.get('/getById/:id', (req, res) => {
-    const employeeId = req.params.id;
-    const foundEmployee = employees.find((employee) => employee.id === employeeId);
-    if(foundEmployee){
-        res.status(200).json({
-            success: true,
-            msg: 'Employee found',
-            data: foundEmployee
-        });
-    } else{
-        res.status(404).json({
-            success: false,
-            msg: 'Employee not found',
-        });
-    }
+  const employeeId = req.params.id;
+  const foundEmployee = employees.find(
+    (employee) => employee.id === employeeId,
+  );
+  if (foundEmployee) {
+    res.status(200).json({
+      success: true,
+      msg: 'Employee found',
+      data: foundEmployee,
+    });
+  } else {
+    res.status(404).json({
+      success: false,
+      msg: 'Employee not found',
+    });
+  }
 });
 
 router.post('/add/', (req, res) => {
-    const newEmployee = req.body;
-    console.log(newEmployee);
-    employees.push(newEmployee);
-    fs.writeFile('src/data/employees.json', JSON.stringify(employees), (err) => {
-        if (err) {
-            res.status(400).json({
-                success: false,
-                msg: 'Bad request',
-            });
-        }else {
-            res.status(201).json({
-                success: true,
-                msg: 'Employee created',
-                newEmployee: newEmployee
-            });
-        }
-    });
+  const newEmployee = req.body;
+  employees.push(newEmployee);
+  fs.writeFile('src/data/employees.json', JSON.stringify(employees), (err) => {
+    if (err) {
+      res.status(400).json({
+        success: false,
+        msg: 'Bad request',
+      });
+    } else {
+      res.status(201).json({
+        success: true,
+        msg: 'Employee created successfully',
+        data: newEmployee,
+      });
+    }
+  });
 });
 
 module.exports = router;
