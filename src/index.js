8b05dc0580113933b43e10e5916b071844957b78
Erick Sequeira <ericknz2014@gmail.com>
2022-10-10 21:15:01 -0300

    TG: add data to json, create metod get, getbyid and post

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
+const express = require ('express');
+
+const app = express();
+const employeesRouter = require('./resources/employees');
+app.use(express.json());
+app.use('/employees', employeesRouter);
+
 // use "import" to import libraries
-import express from 'express';
+//import express from 'express';
 
 // use "require" to import JSON files
 const admins = require('./data/admins.json');
 
-const app = express();
+//const app = express();
 const port = process.env.PORT || 3000;
-
-app.use(express.json());
 
 app.get('/', async (req, res) => {
   res.send('Hello World!');
@@ -23,3 +28,5 @@ app.listen(port, () => {
   // eslint-disable-next-line no-console
   console.log(`Example app listening on port ${port}`);
 });
+
+
