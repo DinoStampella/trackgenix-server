da6d2f584d3054613508a2931708914f9ab237c4
Luciana Prates <lucianaprates10@gmail.com>
2022-10-11 16:03:39 -0300

    TG: code corrections

--- src/resources/super-admins.js
+++ src/resources/super-admins.js
@@ -2,19 +2,16 @@ import express from 'express';
 
 const router = express.Router();
 
-const fs = require('fs');
-
 const superAdmins = require('../data/super-admins.json');
 
 router.get('/filter', (req, res) => {
-    const firstName = req.query.firstName;
-    const foundSuperAdmins = superAdmins.filter((superAdmin) => superAdmin.firstName === firstName);
-    console.log(firstName)
-    if(foundSuperAdmins) {
-        res.status(200).send({ success: true, msg: 'Super Admin found successfully', data: foundSuperAdmins });
-    } else {
-        res.status(404).send({ success: false });
-    }
-})
+  const { firstName } = req.query;
+  const foundSuperAdmins = superAdmins.filter((superAdmin) => superAdmin.firstName === firstName);
+  if (foundSuperAdmins) {
+    res.status(200).send({ success: true, msg: 'Super Admin found successfully', data: foundSuperAdmins });
+  } else {
+    res.status(404).send({ success: false });
+  }
+});
 
 export default router;
