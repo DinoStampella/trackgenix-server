e0af59cf4931dd84e4190b5d3f531e0476842b08
Luciana Prates <lucianaprates10@gmail.com>
2022-10-11 19:43:25 -0300

    TG-08: correction in the request

--- src/resources/tasks.js
+++ src/resources/tasks.js
@@ -7,10 +7,10 @@ const tasks = require('../data/tasks.json');
 router.get('/filter', (req, res) => {
   const { taskName } = req.query;
   const foundTask = tasks.filter((task) => task.taskName === taskName);
-  if (foundTask) {
-    res.send(foundTask);
+  if (foundTask.length > 0) {
+    res.send({ success: true, msg: 'Task found successfully', data: foundTask });
   } else {
-    res.status(404).send('{success: false,msg: "Task not found"}');
+    res.status(400).send({ success: false });
   }
 });
 
