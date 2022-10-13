5fdae85c0e2309775ba84dcf4a025566e171e569
Luciana Prates <lucianaprates10@gmail.com>
2022-10-13 00:24:15 -0300

    TG-08: revision fixes

--- src/resources/super-admins.js
+++ src/resources/super-admins.js
@@ -7,7 +7,7 @@ const superAdmins = require('../data/super-admins.json');
 router.get('/:firstName', (req, res) => {
   const { firstName } = req.params;
   const foundSuperAdmins = superAdmins.filter((superAdmin) => superAdmin.firstName === firstName);
-  if (foundSuperAdmins[0]) {
+  if (foundSuperAdmins.length > 0) {
     res.status(200).json({ success: true, msg: 'Super Admin found successfully', data: foundSuperAdmins });
   } else {
     res.status(404).json({ success: false, msg: 'There is no super admin' });
