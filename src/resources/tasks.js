2e1ae5c1691da90062666e6c2fcc43a09e5acf04
Luciana Prates <lucianaprates10@gmail.com>
2022-10-12 22:47:40 -0300

    TG-08: revision fixes

--- src/resources/tasks.js
+++ src/resources/tasks.js
@@ -4,13 +4,13 @@ const router = express.Router();
 
 const tasks = require('../data/tasks.json');
 
-router.get('/getById/:id', (req, res) => {
+router.get('/:id', (req, res) => {
   const taskId = parseInt(req.params.id, 10);
   const foundTask = tasks.find((task) => task.id === taskId);
   if (foundTask) {
-    res.status(200).send({ success: true, msg: 'Task found successfully', data: foundTask });
+    res.status(200).json({ success: true, msg: 'Task found successfully', data: foundTask });
   } else {
-    res.status(404).send({ success: false, msg: 'There is no task with this id' });
+    res.status(404).json({ success: false, msg: 'There is no task with this id' });
   }
 });
 
