b9e6cbbfeeae1185653a7988e66396629c6ab4ae
Maicol Freire <maicolfreire001@gmail.com>
2022-10-14 13:51:07 -0300

    TG-2: Added connection to database

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import express from 'express';
-
+import mongoose from 'mongoose';
 import adminsRouter from './controllers/admins';
 import superAdminsRouter from './controllers/super-admins';
 import projectsRouter from './controllers/projects';
@@ -14,10 +14,6 @@ const port = process.env.PORT || 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.get('/', async (req, res) => {
-  res.send('Hello World!');
-});
-
 app.use('/admins', adminsRouter);
 app.use('/super-admins', superAdminsRouter);
 app.use('/projects', projectsRouter);
@@ -25,7 +21,18 @@ app.use('/tasks', tasksRouter);
 app.use('/time-sheets', timeSheetsRouter);
 app.use('/employees', employeesRouter);
 
-app.listen(port, () => {
-  // eslint-disable-next-line no-console
-  console.log(`Example app listening on port ${port}`);
-});
+const MONGO_URL = 'mongodb+srv://grupo-a:QWrYuBY4E4MCAo1q@cluster0.ww0uoal.mongodb.net/?retryWrites=true&w=majority';
+
+mongoose.connect(
+  MONGO_URL,
+  (error) => {
+    if (error) {
+      console.log('Failed connection to database', error);
+    } else {
+      console.log('Connected to database');
+      app.listen(port, () => {
+        console.log(`Server ready on port ${port}`);
+      });
+    }
+  },
+);
