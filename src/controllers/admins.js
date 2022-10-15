3d2f1be896bfe3e0b533e9623cb1cd172b53f527
Maicol Freire <maicolfreire001@gmail.com>
2022-10-15 12:04:24 -0300

    TG-3: Added PUT request

--- src/controllers/admins.js
+++ src/controllers/admins.js
@@ -19,6 +19,30 @@ const deleteAdmin = async (req, res) => {
   }
 };
 
+const modifyAdmin = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const result = await Admins.findByIdAndUpdate(
+      { _id: id },
+      { ...req.body },
+      { new: true },
+    );
+
+    return res.status(200).json({
+      message: `Admin with id ${id} modified.`,
+      data: result,
+      error: false,
+    });
+  } catch (error) {
+    return res.status(400).json({
+      message: error,
+      data: undefined,
+      error: true,
+    });
+  }
+}
+
 export default {
   deleteAdmin,
+  modifyAdmin,
 };
