52393201d3f9abc61f3948f37dfa1cbcd69d5124
Francisco Santarelli <franciscosantarelli@gmail.com>
2022-10-16 10:45:57 -0300

    TG-5: Add the employee model and the routes

---
+++ src/routes/employees.js
@@ -0,0 +1,11 @@
+import express from 'express';
+import employeesConstrollers from '../controllers/employees';
+// Faltan validaciones
+
+const router = express.Router();
+
+router
+  .delete('/:id', employeesConstrollers.deleteEmployee)
+  .put('/:id', employeesConstrollers.editEmployee);
+
+export default router;
