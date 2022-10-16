0abaceffca52ae7af90f5ded882d97e536ad1b96
unknown <jorgelinayapur1994@gmail.com>
2022-10-16 10:37:00 -0300

    TG-4: Change validations and schema

--- src/validations/employees.js
+++ src/validations/employees.js
@@ -2,8 +2,10 @@ import Joi from 'joi';
 
 const validateCreation = (req, res, next) => {
   const employeeValidation = Joi.object({
-    firstName: Joi.string().min(2).max(30).required(),
-    lastName: Joi.string().min(2).max(30).required(),
+    firstName: Joi.string().regex('/^[a-zA-Z]+$/').min(2).max(30)
+      .required(),
+    lastName: Joi.string().regex('/^[a-zA-Z]+$/').min(2).max(30)
+      .required(),
     email: Joi.string().email().required(),
     password: Joi.string().alphanum().min(8).max(50)
       .required(),
