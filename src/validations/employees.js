71bdb95db31284a34502c04f8287b4657a720661
Francisco Santarelli <franciscosantarelli@gmail.com>
2022-10-16 18:53:39 -0300

    TG-5: Add the postman collection

--- src/validations/employees.js
+++ src/validations/employees.js
@@ -2,9 +2,9 @@ import Joi from 'joi';
 
 const validateCreation = (req, res, next) => {
   const employeeValidation = Joi.object({
-    firstName: Joi.string().regex(/^[a-zA-Z]+$/).min(2).max(50)
+    firstName: Joi.string().regex(/^[a-zA-Z]+$/).min(2).max(30)
       .required(),
-    lastName: Joi.string().regex(/^[a-zA-Z]+$/).min(2).max(50)
+    lastName: Joi.string().regex(/^[a-zA-Z]+$/).min(2).max(30)
       .required(),
     email: Joi.string().email().required(),
     password: Joi.string().alphanum().min(8).max(50)
