31a2a9ed18625a1a959656726d9fc6996de60b30
Luciana Prates <lucianaprates10@gmail.com>
2022-10-17 18:54:20 -0300

    TG-11: delete and update task implementation

---
+++ src/models/Projects.js
@@ -0,0 +1,9 @@
+import mongoose from 'mongoose';
+
+const taskSchema = new mongoose.Schema({
+  description: { type: String,
+    enum: ['Frontend', 'Backend', 'Testing'],
+    require: true },
+});
+
+export default mongoose.model('Project', taskSchema);
