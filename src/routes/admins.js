245c8f6fd53fa810b4fd5e00c26789fa304c5cc5
Maicol Freire <maicolfreire001@gmail.com>
2022-10-17 07:58:40 -0300

    TG-3: Edited validations

--- src/routes/admins.js
+++ src/routes/admins.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import adminControllers from '../controllers/admins';
+import adminValidations from '../validations/admins';
 
 const router = express.Router();
 
 router.delete('/:id', adminControllers.deleteAdmin);
-router.put('/:id', adminControllers.modifyAdmin);
+router.put('/:id', adminValidations.validateCreation, adminControllers.modifyAdmin);
 
 export default router;
--- src/validations/admins.js
+++ src/validations/admins.js
@@ -2,15 +2,15 @@ import Joi from 'joi';
 
 const validateCreation = (req, res, next) => {
   const adminValidation = Joi.object({
-    firstName: Joi.required().regex(/^[a-zA-Z]+$/).string().min(2)
-      .max(30),
-    lastName: Joi.required().regex(/^[a-zA-Z]+$/).string().min(2)
-      .max(30),
-    email: Joi.required().string().email(),
-    password: Joi.required().string().alphanum().min(8)
-      .max(50),
-    dni: Joi.required().string().regex(/^\d+$/).min(7)
-      .max(11),
+    firstName: Joi.string().regex(/^[a-zA-Z]+$/).min(2).max(30)
+      .required(),
+    lastName: Joi.string().regex(/^[a-zA-Z]+$/).min(2).max(30)
+      .required(),
+    email: Joi.string().required().email(),
+    password: Joi.string().alphanum().min(8).max(50)
+      .required(),
+    dni: Joi.string().regex(/^\d+$/).min(7).max(11)
+      .required(),
     phone: Joi.string().regex(/^\d+$/).min(8).max(15),
     location: Joi.string().min(3).max(50),
   });
