69877ba88e66b162a04267e3a832826506d0fc05
Erick Sequeira <ericknz2014@gmail.com>
2022-10-18 23:10:37 -0300

    TG-12: resolved comments

--- src/controllers/timesheets.js
+++ src/controllers/timesheets.js
@@ -1,35 +1,61 @@
 import Timesheets from '../models/Timesheets';
 
+const { ObjectId } = require('mongoose').Types;
+
+function isValidObjectId(id) {
+  if (ObjectId.isValid(id)) {
+    if ((String)(new ObjectId(id)) === id) { return true; }
+    return false;
+  }
+  return false;
+}
+
 const getAllTimesheets = async (req, res) => {
   try {
     const timesheets = await Timesheets.find();
-
+    if (timesheets.length === 0) {
+      return res.status(404).json({
+        message: 'Timesheet Not Found',
+        error: true,
+      });
+    }
     return res.status(200).json({
       message: 'Timesheet Found',
       data: timesheets,
       error: false,
     });
-  } catch {
-    return res.status(404).json({
-      message: 'Timesheet Not Found',
+  } catch (error) {
+    return res.status(500).json({
+      message: `Unexpected error ${error}`,
       error: true,
     });
   }
 };
 
-const getAllTimesheetById = async (req, res) => {
+const getTimesheetById = async (req, res) => {
   try {
     const timesheetId = req.params.id;
+    if (!isValidObjectId(timesheetId)) {
+      return res.status(400).json({
+        message: `Invalid id: ${req.params.id}`,
+        error: true,
+      });
+    }
     const timesheets = await Timesheets.findById(timesheetId);
-
+    if (!timesheets) {
+      return res.status(404).json({
+        message: 'Timesheet Not Found',
+        error: true,
+      });
+    }
     return res.status(200).json({
       message: 'Timesheet Found',
       data: timesheets,
       error: false,
     });
-  } catch {
-    return res.status(404).json({
-      message: 'Timesheet Not Found',
+  } catch (error) {
+    return res.status(500).json({
+      message: `Unexpected error ${error}`,
       error: true,
     });
   }
@@ -37,22 +63,21 @@ const getAllTimesheetById = async (req, res) => {
 
 const createTimesheet = async (req, res) => {
   try {
-    const requestTimesheet = req.body;
-    const timesheets = await Timesheets.create(requestTimesheet);
+    const timesheets = await Timesheets.create(req.body);
 
     return res.status(201).json({
       message: 'Timesheet Created',
       data: timesheets,
       error: false,
     });
-  } catch {
-    return res.status(400).json({
-      message: 'Can Not Create Timesheet',
+  } catch (error) {
+    return res.status(500).json({
+      message: `Unexpected error ${error}`,
       error: true,
     });
   }
 };
 
 export default {
-  getAllTimesheets, getAllTimesheetById, createTimesheet,
+  getAllTimesheets, getTimesheetById, createTimesheet,
 };
--- src/validations/timesheets.js
+++ src/validations/timesheets.js
@@ -2,16 +2,32 @@ import Joi from 'joi';
 
 const validateCreation = (req, res, next) => {
   const timesheetValidations = Joi.object({
-    date: Joi.date().required().max(new Date()),
-    task: Joi.string().min(3).max(20).required(),
-    description: Joi.string().min(3).max(150).required(),
+    date: Joi.date().required().max(new Date()).messages({
+      'string.empty': 'Date required',
+      'date.pattern.base': 'Date must be after today',
+      'any.required': 'Date required',
+    }),
+    task: Joi.string().min(3).max(20).required()
+      .messages({
+        'string.empty': 'Task required',
+        'string.min': 'Task should have a minimum length of 3 characters',
+        'string.max': 'Task should have a maximum length of 20 characters',
+        'any.required': 'Task required',
+      }),
+    description: Joi.string().min(3).max(150).required()
+      .messages({
+        'string.empty': 'Description required',
+        'string.min': 'Description should have a minimum length of 3 characters',
+        'string.max': 'Description should have a maximum length of 150 characters',
+        'any.required': 'Description required',
+      }),
   });
 
-  const validation = timesheetValidations.validate(req.body);
+  const validation = timesheetValidations.validate(req.body, { abortEarly: false });
 
   if (validation.error) {
     return res.status(400).json({
-      message: `There was an error :${validation.error.details[0].message}`,
+      message: validation.error.details,
       date: undefined,
       error: true,
     });
