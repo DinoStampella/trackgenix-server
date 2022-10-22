02905c24f86e41a265dc933ac51e512b8e734f6c
Francisco Santarelli <franciscosantarelli@gmail.com>
2022-10-22 20:45:24 -0300

    TG-33: Add more tests for DELETE

--- src/test/timesheets.test.js
+++ src/test/timesheets.test.js
@@ -8,6 +8,8 @@ beforeAll(async () => {
 });
 
 const timeSheetId = '63531fd7410c845909ab22e7';
+const fakeTimeSheetId = '63531fd7410c845909ab22e6';
+const wrongTimeSheetId = '1234fds';
 
 // const mockedTimeSheet = {
 //   date: '20-04-2022',
@@ -19,5 +21,31 @@ describe('DELETE /timesheet', () => {
   test('Test status code success delete', async () => {
     const response = await request(app).delete(`/time-sheets/${timeSheetId}`).send();
     expect(response.status).toBe(204);
+  });
+
+  test('Test status code failure delete (wrong id)', async () => {
+    const response = await request(app).delete(`/time-sheets/${wrongTimeSheetId}`).send();
+    expect(response.status).toBe(400);
+  });
+  test('Test error in failure delete (wrong id)', async () => {
+    const response = await request(app).delete(`/time-sheets/${wrongTimeSheetId}`).send();
+    expect(response.body.error).toBeTruthy();
+  });
+  test('Test error messagge contain id in failure delete (wrong id)', async () => {
+    const response = await request(app).delete(`/time-sheets/${wrongTimeSheetId}`).send();
+    expect(response.body.message).toContain(`${wrongTimeSheetId}`);
+  });
+
+  test('Test status code failure delete (no employee found)', async () => {
+    const response = await request(app).delete(`/time-sheets/${fakeTimeSheetId}`).send();
+    expect(response.status).toBe(404);
+  });
+  test('Test error in failure delete (wrong id)', async () => {
+    const response = await request(app).delete(`/time-sheets/${fakeTimeSheetId}`).send();
+    expect(response.body.error).toBeTruthy();
+  });
+  test('Test error messagge contain id in failure delete (wrong id)', async () => {
+    const response = await request(app).delete(`/time-sheets/${fakeTimeSheetId}`).send();
+    expect(response.body.message).toContain(`${fakeTimeSheetId}`);
   });
 });
