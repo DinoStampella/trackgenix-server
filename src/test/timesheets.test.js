7a8a0979c83f4ea0cbd06abd9e282c7060c0d4ad
Francisco Santarelli <franciscosantarelli@gmail.com>
2022-10-22 20:07:17 -0300

    TG-33: Create the test for DELETE

--- src/test/timesheets.test.js
+++ src/test/timesheets.test.js
@@ -1,1 +1,23 @@
-test.skip('skip', () => {});
+import request from 'supertest';
+import app from '../app';
+import TimeSheet from '../models/Timesheets';
+import timeSheetSeed from '../seed/timesheets';
+
+beforeAll(async () => {
+  await TimeSheet.collection.insertMany(timeSheetSeed);
+});
+
+const timeSheetId = '63531fd7410c845909ab22e7';
+
+// const mockedTimeSheet = {
+//   date: '20-04-2022',
+//   description: 'A description',
+//   hours: 300,
+// };
+
+describe('DELETE /timesheet', () => {
+  test('Test status code success delete', async () => {
+    const response = await request(app).delete(`/time-sheets/${timeSheetId}`).send();
+    expect(response.status).toBe(204);
+  });
+});
