d78e9bd8c5372462d273bd13e67d6db47f81b14a
JuanModica <jcmodica18@gmail.com>
2022-10-24 12:49:23 -0300

    TG-26: testing on GET employee

--- src/test/employees.test.js
+++ src/test/employees.test.js
@@ -1,1 +1,31 @@
-test.skip('skip', () => {});
+import request from 'supertest';
+import app from '../app';
+import Employee from '../models/Employees';
+import employeesSeed from '../seed/employees';
+
+beforeAll(async () => {
+  await Employee.collection.insertMany(employeesSeed);
+});
+
+describe('GET /employees', () => {
+  test('should return status code 200', async () => {
+    const response = await request(app).get('/employees').send();
+
+    expect(response.status).toBe(200);
+  });
+  test('should return message Employees found', async () => {
+    const response = await request(app).get('/employees').send();
+
+    expect(response.body.message).toBe('Employees found');
+  });
+  test('should return error false', async () => {
+    const response = await request(app).get('/employees').send();
+
+    expect(response.body.data.length).toBeGreaterThan(0);
+  });
+  test('should return error false', async () => {
+    const response = await request(app).get('/employees').send();
+
+    expect(response.body.error).toBeFalsy();
+  });
+});
