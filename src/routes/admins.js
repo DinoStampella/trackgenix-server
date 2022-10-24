da31247cc7185f5e0ae29fa547a169fa5451385d
Luciana Prates <lucianaprates10@gmail.com>
2022-10-23 21:32:15 -0300

    TG-28: Test Super Admin to get endpoint

--- src/routes/admins.js
+++ src/routes/admins.js
@@ -8,6 +8,6 @@ router.get('/', adminControllers.getAllAdmins);
 router.get('/:id', adminControllers.getAdminById);
 router.post('/', userValidations.validateCreation, adminControllers.createAdmin);
 router.delete('/:id', adminControllers.deleteAdmin);
-router.put('/:id', userValidations.validateCreation, adminControllers.modifyAdmin);
+router.put('/:id', userValidations.validateCreation, adminControllers.updateAdmin);
 
 export default router;
--- src/test/super-admins.test.js
+++ src/test/super-admins.test.js
@@ -1,1 +1,26 @@
-test.skip('skip', () => {});
+import request from 'supertest';
+import app from '../app';
+import SuperAdmins from '../models/Super-admins';
+import superAdminsSeed from '../seed/super-admin';
+
+beforeAll(async () => {
+  await SuperAdmins.collection.insertMany(superAdminsSeed);
+});
+
+describe('GET /super-admins', () => {
+  test('sholud return status code 200', async () => {
+    const response = await request(app).get('/super-admins').send();
+
+    expect(response.status).toBe(200);
+  });
+  test('sholud return error false', async () => {
+    const response = await request(app).get('/super-admins').send();
+
+    expect(response.body.error).toBeFalsy();
+  });
+  test('sholud return more than one employee', async () => {
+    const response = await request(app).get('/super-admins').send();
+
+    expect(response.body.data.length).toBeGreaterThan(0);
+  });
+});
