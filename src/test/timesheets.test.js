b5a0ee31f600219e4822a3d443ae54d85afa369e
unknown <jorgelinayapur1994@gmail.com>
2022-10-24 18:01:04 -0300

    TG-32:Add get test for timesheets

--- src/test/timesheets.test.js
+++ src/test/timesheets.test.js
@@ -1,1 +1,15 @@
-test.skip('skip', () => {});
+import request from 'supertest';
+import app from '../app';
+import Timesheets from '../models/Timesheets';
+import timesheetsSeed from '../seed/timesheets';
+
+beforeAll(async () => {
+  await Timesheets.collection.insertMany(timesheetsSeed);
+});
+
+describe('GET /time-sheets', () => {
+  test('Shoud return status code 200', async () => {
+    const response = await request(app).get('/time-sheets').send();
+    expect(response.status).toBe(200);
+  });
+});
