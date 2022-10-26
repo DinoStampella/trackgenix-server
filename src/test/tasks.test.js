3208b0e30fbb16f0089ffeb66594281bb9ec409a
Erick Sequeira <ericknz2014@gmail.com>
2022-10-26 01:16:19 -0300

    TG-25: resolve comments

--- src/test/tasks.test.js
+++ src/test/tasks.test.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-underscore-dangle */
 import request from 'supertest';
 import app from '../app';
 import Task from '../models/Tasks';
@@ -15,9 +16,10 @@ const mockedTaskInvalid = {
   description: 'Apple',
 };
 
-const BackendTask = '63531a7c73636855c2aa7f9a';
-const FrontendTask = '6357795eb78425a9d8da6849';
+const firstTaskId = Taskseed[0]._id;
+const secondTaskId = Taskseed[1]._id;
 const invalidId = 123;
+const invalidTaskId = '62731244ec6456efd12685ef';
 
 beforeAll(async () => {
   await Task.collection.insertMany(Taskseed);
@@ -25,7 +27,7 @@ beforeAll(async () => {
 
 describe('Delete/task', () => {
   test('should return status code 204', async () => {
-    const response = await request(app).delete(`/tasks/${BackendTask}`).send();
+    const response = await request(app).delete(`/tasks/${firstTaskId}`).send();
     expect(response.status).toBe(204);
   });
   test('should return 400', async () => {
@@ -35,36 +37,37 @@ describe('Delete/task', () => {
     expect(response.body.message).toBe(`Invalid id ${invalidId}`);
   });
   test('should return 404', async () => {
-    const response = await request(app).delete(`/tasks/${BackendTask}`).send();
+    const response = await request(app).delete(`/tasks/${firstTaskId}`).send();
     expect(response.status).toBe(404);
     expect(response.body.error).toBeTruthy();
-    expect(response.body.message).toBe(`Couldn't find task with id ${BackendTask}`);
+    expect(response.body.message).toBe(`Couldn't find task with id ${firstTaskId}`);
   });
 });
 
 describe('Put/employees', () => {
   test('should return status code 200', async () => {
-    const response = await request(app).put(`/tasks/${FrontendTask}`).send(mockedTaskModified);
+    const response = await request(app).put(`/tasks/${secondTaskId}`).send(mockedTaskModified);
     expect(response.status).toBe(200);
     expect(response.body.data.description).toBe('Testing');
     expect(response.body.error).toBeFalsy();
-    expect(response.body.message).toBe(`Modified task with id ${FrontendTask}`);
+    expect(response.body.message).toBe(`Modified task with id ${secondTaskId}`);
   });
   test('should return status code 400', async () => {
     const response = await request(app).put(`/tasks/${invalidId}`).send(mockedTask);
     expect(response.status).toBe(400);
+    expect(response.body.error).toBeTruthy();
     expect(response.body.message).toBe(`Invalid id ${invalidId}`);
   });
   test('should return status code 404', async () => {
-    const idThatNotexist = '62731244ec6456efd12685ef';
-    const response = await request(app).put(`/tasks/${idThatNotexist}`).send(mockedTask);
+    const response = await request(app).put(`/tasks/${invalidTaskId}`).send(mockedTask);
     expect(response.status).toBe(404);
     expect(response.body.error).toBeTruthy();
-    expect(response.body.message).toBe(`Couldn't find task with id ${idThatNotexist}`);
+    expect(response.body.message).toBe(`Couldn't find task with id ${invalidTaskId}`);
   });
   test('should return status code 400 validate error', async () => {
-    const response = await request(app).put(`/tasks/${FrontendTask}`).send(mockedTaskInvalid);
+    const response = await request(app).put(`/tasks/${secondTaskId}`).send(mockedTaskInvalid);
     expect(response.status).toBe(400);
     expect(response.body.message[0].message).toBe('Description should be Frontend, Backend or Testing');
+    expect(response.error).toBeTruthy();
   });
 });
