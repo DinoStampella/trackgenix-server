cd69ec1d0b0f60f51b5a678a2e765b097727a928
Julian Flores <julianfloresdev@gmail.com>
2022-12-14 16:54:15 -0300

    TG-89: add validation to allow employee PM push changes

--- src/routes/projects.js
+++ src/routes/projects.js
@@ -10,8 +10,8 @@ const router = express.Router();
 router
   .get('/', checkAuth(['super-admin', 'admin', 'employee']), getAllProjects)
   .get('/:id', checkAuth(['super-admin', 'admin', 'employee']), getProjectById)
-  .post('/', checkAuth(['super-admin', 'admin']), validateProject, createProject)
-  .put('/:id', checkAuth(['super-admin', 'admin']), validateProject, updateProject)
+  .post('/', checkAuth(['super-admin', 'admin', 'employee']), validateProject, createProject)
+  .put('/:id', checkAuth(['super-admin', 'admin', 'employee']), validateProject, updateProject)
   .delete('/:id', checkAuth(['super-admin', 'admin']), deleteProject);
 
 export default router;
